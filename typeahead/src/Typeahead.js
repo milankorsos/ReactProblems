abeb48e5b85a7728884a65bd914b3af3b1bc7b19
Milan Korsos <milan.korsos@hgdata.com>
2017-04-04 13:22:25 -0700

    Improve typeahead

--- typeahead/src/Typeahead.js
+++ typeahead/src/Typeahead.js
@@ -2,35 +2,55 @@ import React, { Component } from 'react';
 import './Typeahead.css';
 
 class ResultItem extends Component {
-  onClick(item) {
-    window.open(`https://twitter.com/${item.screen_name}`);
+  onClick(screen_name) {
+    window.open(`https://twitter.com/${screen_name}`);
   }
 
   render() {
-    const { item } = this.props;
+    const {
+      item: {
+        screen_name,
+        name,
+        description
+      },
+      index,
+      selectedIndex,
+      q
+    } = this.props;
+
+    const qIndex = screen_name.toLowerCase().indexOf(q.toLowerCase());
+    const head = screen_name.slice(0, qIndex);
+    const tail = screen_name.slice(qIndex + q.length);
+
+    const className = selectedIndex === index ? 'selected' : '';
     return (
-        <li onClick={ this.onClick.bind(this, item) }>
+        <li
+          onClick={ this.onClick.bind(this, screen_name) }
+          className={ className }
+        >
           <div>
-            <span className="name">{ item.name }</span>
-            <span>@{ item.screen_name }</span>
+            <span className="name">{ name }</span>
+            <span>@{ head }<strong>{ q }</strong>{ tail }</span>
           </div>
-          <div>{ item.description }</div>
+          <div>{ description }</div>
         </li>
     );
   }
 }
 
 class ResultsList extends Component {
+
   render() {
     const { filteredResults } = this.props;
-    console.log('filteredResults', filteredResults)
     return (
       <ul>
       {
-        filteredResults.map(item => {
+        filteredResults.map((item, index) => {
           return <ResultItem
             item={ item }
+            index={ index }
             key={ item.id }
+            {...this.props}
           />
         })
       }
@@ -57,8 +77,13 @@ class Typeahead extends Component {
     super(props);
 
     this.state = {
-      filteredResults: []
+      q: '',
+      filteredResults: [],
+      selectedIndex: -1
     };
+
+    window.addEventListener('keyup', this.handleKeyUp.bind(this), false);
+
   }
 
   onChange(event) {
@@ -69,23 +94,54 @@ class Typeahead extends Component {
     if (q.length) {
       filteredResults = results.filter(item => {
         const screenName = item.screen_name.toLowerCase();
-        const name = item.name.toLowerCase();
-        return screenName.indexOf(q) !== -1 || name.indexOf(q) !== -1;
+        return screenName.indexOf(q.toLowerCase()) !== -1;
       });
     } else {
       filteredResults = [];
     }
 
-    // this.setState({ q });
-    this.setState({ filteredResults });
+    this.setState({
+      q,
+      filteredResults,
+      selectedIndex: -1
+    });
   }
 
-  render() {
+  handleKeyUp(event) {
     const { filteredResults } = this.state;
+    let { selectedIndex } = this.state;
+
+    switch (event.code) {
+      case 'ArrowUp':
+        if (selectedIndex > -1) {
+          selectedIndex--;
+        }
+        break;
+      case 'ArrowDown':
+        if (selectedIndex < filteredResults.length - 1) {
+          selectedIndex++;
+        }
+        break;
+      case 'Enter':
+        if (selectedIndex !== -1) {
+          const screenName = filteredResults[selectedIndex].screen_name;
+          window.open(`https://twitter.com/${screenName}`);
+        }
+        break;
+      default:
+    }
+    this.setState({ selectedIndex });
+  }
+
+
+  render() {
+    const { filteredResults, q, selectedIndex } = this.state;
     return (
       <div id="typeahead">
         <SearchBar onChange={ this.onChange.bind(this) } />
         <ResultsList
+          q={ q }
+          selectedIndex={ selectedIndex }
           filteredResults={ filteredResults }
           {...this.props}
         />
